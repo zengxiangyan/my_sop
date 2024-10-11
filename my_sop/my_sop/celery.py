990849f926182e7f13100831a220aa4f56a9f08b
zeng.xiangyan <zeng.xiangyan@nint.com>
2024-10-11 11:39:01 +0800

    1

--- my_sop/my_sop/celery.py
+++ my_sop/my_sop/celery.py
@@ -13,6 +13,6 @@ app.autodiscover_tasks()
 # 定义任务路由
 app.conf.task_routes = {
     'my_app.tasks.my_task': {
-        'queue': '10.21.90.130',  # 默认队列，当没有指定的队列时使用
+        'queue': '127.0.0.1',  # 默认队列，当没有指定的队列时使用
     },
 }
